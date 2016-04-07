9a21147b872b0c2b43b785063fbb4cbc942359e6
Sheplu <jean.burellier@gmail.com>
2016-04-08 01:10:10 +0200

    update route

--- routes/favoriteRouter.js
+++ routes/favoriteRouter.js
@@ -10,23 +10,45 @@ favoriteRouter.use(bodyParser.json());
 
 favoriteRouter.route('/')
 .get(Verify.verifyOrdinaryUser, function (req, res, next) {
-    Leaders.find({}, function (err, leader) {
+    Favorites.find({"postedBy": req.decoded._doc._id})
+    .populate('postedBy')
+    .populate('dishes')
+    .exec(function (err, fav) {
         if (err) throw err;
-        res.json(leader);
+        res.json(fav);
     });
 })
 
-.post(Verify.verifyOrdinaryUser, Verify.verifyAdmin, function (req, res, next) {
-    Leaders.create(req.body, function (err, leader) {
-        if (err) throw err;
-        console.log('Leader created!');
-        var id = Leader._id;
+.post(Verify.verifyOrdinaryUser, function (req, res, next) {
+    Favorites.find({"postedBy": req.decoded._doc._id})
+    .exec(function (err, fav) {
+      if(!fav.length) {
+        Favorites.create({postedBy: req.decoded._doc._id, dishes: req.body.id}, function (err, fav) {
+            if (err) throw err;
+            console.log('Favorites created!');
+            var id = fav._id;
 
-        res.writeHead(200, {
-            'Content-Type': 'text/plain'
+            res.writeHead(200, {
+                'Content-Type': 'text/plain'
+            });
+            res.end('Added the favorite with id: ' + id);
         });
-        res.end('Added the favorite with id: ' + id);
+      }
+      else {
+        //console.log(fav[0].dishes);
+
+        fav[0].dishes.push(req.body.id);
+        fav[0].save(function (err, favo) {
+            if (err) throw err;
+            console.log('Updated fav!');
+            res.json(favo);
+        });
+
+      }
     });
+    /*
+
+    */
 })
 
 .delete(Verify.verifyOrdinaryUser, Verify.verifyAdmin, function (req, res, next) {
